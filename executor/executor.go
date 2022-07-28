3a4313060f1a3015afdca6a641469f9206b5b06b
胡标 <biao.hu@baishan.com>
2022-07-28 21:03:01 +0800

    feat: init gin

--- executor/executor.go
+++ executor/executor.go
@@ -9,6 +9,41 @@ import (
 	"syscall"
 )
 
+type (
+	// Service 完整的服务接口
+	Service interface {
+		Starter
+		Stopper
+	}
+
+	// Starter 开始方法的接口
+	Starter interface {
+		Start() error
+	}
+	// Stopper 结束方法的接口
+	Stopper interface {
+		Stop()
+	}
+
+	// MulServices 多服务程序的注册
+	MulServices interface {
+		ServicesRegistration() []Service
+	}
+
+	// Program 完整程序的接口
+	Program interface {
+		Starter
+		Stopper
+	}
+
+	// MulServicesProgram 支持多服务启动及关闭的程序接口
+	MulServicesProgram interface {
+		Starter
+		Stopper
+		MulServices
+	}
+)
+
 // CtrlC 捕获ctrl-c的控制器
 type CtrlC struct {
 	starts       Starter
